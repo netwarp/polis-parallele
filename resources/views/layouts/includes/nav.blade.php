436267faa03acb841fbcc92f04bba4bd90be8a00
netwarp <resethread@gmail.com>
2020-03-21 15:24:24 +0100

    Update nav.blade.php

--- resources/views/layouts/includes/nav.blade.php
+++ resources/views/layouts/includes/nav.blade.php
@@ -46,6 +46,9 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a href="/admin" class="dropdown-item">
+                                Dashboard
+                            </a>
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
