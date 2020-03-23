9cc4c09b0b4f00d7f0b385d71b9b51136d330930
netwarp <resethread@gmail.com>
2020-03-23 15:53:05 +0100

    footer

--- resources/views/layouts/includes/footer.blade.php
+++ resources/views/layouts/includes/footer.blade.php
@@ -1,4 +1,14 @@
 <footer>
-
+    <div class="container">
+        <div class="col">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="{{ action('Front\FrontController@podcasts') }}">Podcasts</a></li>
+                <li><a href="{{ action('Front\FrontController@events') }}">Events</a></li>
+                <li><a href="{{ action('Front\FrontController@support') }}">Support</a></li>
+                <li><a href="{{ action('Front\FrontController@contact') }}">Contact</a></li>
+            </ul>
+        </div>
+    </div>
 </footer>
 @stack('js')
